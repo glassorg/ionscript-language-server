02cba5cc82251b60a0ebc6cd50ba8dde321109c5
Kris Nye <krisnye@gmail.com>
2020-12-15 19:03:23 -0800

    fixed for windows

--- common.js
+++ common.js
@@ -4,7 +4,11 @@ const fs = require("fs");
 var root = null;
 exports.getGlobalModuleRoot = function getGlobalModuleRoot() {
 	if (root == null) {
-		for (let check of ["/usr/local/lib/node_modules/"]) {
+		let possibleRoots = [
+			"/usr/local/lib/node_modules/", // mac
+			`${process.env.APPDATA}\\npm\\node_modules\\` // windows
+		]
+		for (let check of possibleRoots) {
 			if (fs.existsSync(check)) {
 				root = check;
 				break;
