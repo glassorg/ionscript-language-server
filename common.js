ba5e3ee1f63413ddb4dd133354e9477a74084e4b
KodyJKing <kody.j.king@gmail.com>
2021-08-27 22:55:51 -0700

    added /usr/lib/node_modules/ as possible global module root

--- common.js
+++ common.js
@@ -5,7 +5,8 @@ var root = null;
 exports.getGlobalModuleRoot = function getGlobalModuleRoot() {
 	if (root == null) {
 		let possibleRoots = [
-			"/usr/local/lib/node_modules/", // mac
+			"/usr/local/lib/node_modules/", // mac, linux
+			"/usr/lib/node_modules/", // linux (some installations)
 			`${process.env.APPDATA}\\npm\\node_modules\\` // windows
 		]
 		for (let check of possibleRoots) {
